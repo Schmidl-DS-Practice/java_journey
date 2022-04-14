aeaa629a1fbf05da3bb9f163915a5fbe1fd89523
scottschmidl <sjschmidl@gmail.com>
2022-04-14 14:32:40 -0500

    file name and class name need to be the same

---
+++ FirstProgram.java
@@ -0,0 +1,19 @@
+/* This is a
+two-line comment inherited from C */
+
+// This is a comment inherited from C++
+
+/** This form provides information
+ * to the javadoc utility, which will generate documentation
+ * from comments */
+
+// First program
+// SS, 4/14/2022
+
+public class FirstProgram
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Is there anybody out there?");
+    }
+}
