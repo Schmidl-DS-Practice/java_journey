fc38e003cc44a2f4854eda462e925d4fc59584d3
scottschmidl <sjschmidl@gmail.com>
2022-04-18 10:24:24 -0500

    finished 12 and 14, working on 13

--- chapter1/exercises.java
+++ chapter1/exercises.java
@@ -7,37 +7,41 @@ public class Exercises
     public static void main(String [] args)
     {
         // IN THEORY
-        q111();
-        q112();
+        // q111();
+        // q112();
         q113();
-        q114();
+        // int x = 0;
+        // q114(x);
+        // System.out.println(x); // prints 0
+
         // IN PRACTICE
         q115();
         q116();
         q117();
         q118();
+
         // PROGRAMMING PROJECTS
         q119();
         q120();
         q121();
         q122();
     }
-/*
-IN SHORT
-1.1) java and .javac
-1.2) two or more line comment, single-line comment, doc string
-1.3) byte, short, int, long, float, double, char, boolean
-1.4) * performs multiplication between two numbers, *= increments the current number by the value given after the =
-1.5) In Java, an operator applied to an expression yields an expression that has a value. Prefix increment - the semantics of ++x is that the value of the expression is the new value of x. Postfix increment - x++ means that the value of the expression is the original value of x.
-1.6) for(initialization; test; update), while (expression), do{prompt user; read value;} while (value is no good);
-1.7) break statement will break the loop if a condition is met, labeled break statement can use to break out of a nested loop
-1.8) continue statement will move on to the next iteration of the loop without performing an action
-1.9) method overloading means that you can have two methods in the same class, as long as they take at least one different type of arg
-1.10) the actual arguments are copied into the formal parameters. Variables are passed using call-by-value.
-*/
+    /*
+    IN SHORT
+    1.1) java and .javac
+    1.2) two or more line comment, single-line comment, doc string
+    1.3) byte, short, int, long, float, double, char, boolean
+    1.4) * performs multiplication between two numbers, *= increments the current number by the value given after the =
+    1.5) In Java, an operator applied to an expression yields an expression that has a value. Prefix increment - the semantics of ++x is that the value of the expression is the new value of x. Postfix increment - x++ means that the value of the expression is the original value of x.
+    1.6) for(initialization; test; update), while (expression), do{prompt user; read value;} while (value is no good);
+    1.7) break statement will break the loop if a condition is met, labeled break statement can use to break out of a nested loop
+    1.8) continue statement will move on to the next iteration of the loop without performing an action
+    1.9) method overloading means that you can have two methods in the same class, as long as they take at least one different type of arg
+    1.10) the actual arguments are copied into the formal parameters. Variables are passed using call-by-value.
+    */
 
-// IN THEORY
-// 1.11
+    // IN THEORY
+    // 1.11
     public static void q111()
     {
         int b = 5, c = 8, a;
@@ -50,61 +54,73 @@ IN SHORT
         a = ++b + ++c; // b and c are both incremeted before, b = 9 and c = 12
         System.out.println(a); // a = 21, b = 9, c = 12
     }
-// 1.12)
-    public static void q112()
+    // 1.12)
+    public static void q112() // true
     {
-
+        System.out.println(true && false || true);
     }
-// 1.13)
+    // 1.13)
     public static void q113()
     {
+        for (int i = 0; i <= 20; i++)
+        {
+            if (i == 13)
+                continue;
+        }
 
+        int i = 0;
+        while (i <= 20)
+        {
+            if (i == 13)
+                continue;
+            i++;
+        }
     }
-// 1.14)
-    public static void q114()
+    // 1.14)
+    public static void q114(int x) // does nothing
     {
-
+        /* body unknown */
     }
 
-// IN PRACTICE
-// 1.15)
+    // IN PRACTICE
+    // 1.15)
     public static void q115()
         {
 
         }
-// 1.16)
+    // 1.16)
     public static void q116()
         {
 
         }
-// 1.17)
+    // 1.17)
     public static void q117()
         {
 
         }
-// 1.18)
+    // 1.18)
     public static void q118()
         {
 
         }
 
-// PROGRAMMING PROJECTS
-// 1.19)
+    // PROGRAMMING PROJECTS
+    // 1.19)
     public static void q119()
     {
 
     }
-// 1.20)
-public static void q120()
+    // 1.20)
+    public static void q120()
     {
 
     }
-// 1.21)
+    // 1.21)
     public static void q121()
     {
 
     }
-// 1.22)
+    // 1.22)
     public static void q122()
     {
 
